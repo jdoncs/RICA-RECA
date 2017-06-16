2dcc662d89a1fb3b00c0ea137dc27083d33ddc33
Jake Maranzatto <maranzatto1@yahoo.com>
2017-06-16 15:49:31 -0400

    Preliminary RECA implementation
    First attempt at implementing RECA, with 100 agents and 120 resources.  Works when A = R, as well as when A < R.
    -Need to do runtime evaluations
    -not entirely the same algorithm as in the paper, which could cause issues

--- Summer Project.py
+++ Summer Project.py
@@ -21,20 +21,21 @@ def resourceAllocation(a, r, kValue, seed):
     for resource in range(r):
         resources.append(resource)
     myFile = open('new.txt', 'w+')
+    myFile.write('Agents: ' + str(a) + ', Resources: ' + str(r) + ', K Value: ' + str(kValue) + '\n') #JM made data representation the same as Selena's
     random.seed(seed)
     for agent in agents:
         agentResources = randomResources(resources, kValue)
-        myFile.write('Agent ' + str(agent) + ' ---> ')
         for resource in agentResources:
-            myFile.write('Resource ' + str(resource) + ',' + ' ')
+            myFile.write('Resource ' + str(resource) + ' ')
         myFile.write('\n')
         #x = x + 1   #JD: This is probably going going to lead to highly non-random behaviours and should be removed.
-        #JM seed changer removed
+                          #JM seed changer removed
     myFile.close()
 
 def resourceChecker(r, kValue):
-    #CREATING LIST OF DATA
+    #creating data
     myFile = open('new.txt', 'r')
+    next(myFile) #JM skips first line of file, which contains a, r, K information
     values = []
     indices = []
     for i in range(r):
@@ -43,14 +44,14 @@ def resourceChecker(r, kValue):
         
     for line in myFile:
         for j in range(r):
-            if ('Resource ' + str(j) + ',') in line:
+            if ('Resource ' + str(j) + ' ') in line:
                 values[j] += 1
                 
-   #STATISTICS
+   #stats
     mean = statistics.mean(values)
     standard_deviation = statistics.pstdev(values)
     
-   #GRAPHING
+   #graphing
     fig = plt.figure()
     y_pos = np.arange(len(values))
     plt.bar(y_pos, values, align = 'center', alpha = 0.5)
@@ -59,11 +60,55 @@ def resourceChecker(r, kValue):
     plt.title('Distribution of object Occurances at k =' + str(kValue))
     plt.text(50, 20, ('Mean: ' + str(mean)))
     plt.text(50, 10, ('SD: ' + str(standard_deviation)))
-    #JM saving output of data as svg file.  
+    #JM saving output of graph as svg file.  
     fig.savefig('data.svg')
-    plt.show()
+    # plt.show() JM we know this works, so editing out for now
+
+def RECA(preferenceFile): #JM implementing RECA
+    myFile = open(preferenceFile, 'r')
+    next(myFile) #skips the first line of file
+    assignedObjects = []
+    unassignedAgents = []
+    eqClasses = {}
+    currentAgent = 0
+    
+    #making dictionary of prefrences
+    for line in myFile:
+        if line.rstrip(' \n') in eqClasses:
+            eqClasses[line.rstrip(' \n')].append(currentAgent)
+        else:
+            eqClasses[line.rstrip(' \n')] = [currentAgent]  
+        currentAgent += 1
+        
+    #making list of total resources   
+    resourceNumbers = []
+    for i in range(currentAgent):
+        resourceNumbers.append(i)
+    assignments = open('RECA_Assignments.txt', 'w+')    
+    for key in eqClasses:
+        if len(eqClasses[key]) == 1:
+            assignments.write(str(key) + ': ' + str(eqClasses[key][0]) + '\n')
+            #del eqClasses[key]
+            assignedObjects.append(int(key.strip('Resource ')))
+        else:
+            assignedAgent = random.choice(eqClasses[key])
+            assignments.write(str(key) + ': ' + str(assignedAgent) + '\n')
+            eqClasses[key].remove(assignedAgent)
+            assignedObjects.append(int(key.strip('Resource ')))
+            for agent in eqClasses[key]:
+                unassignedAgents.append(agent)
+                
+    #printing remaining assignments
+    unassignedObjects = [x for x in resourceNumbers if x not in assignedObjects]
+    random.shuffle(unassignedObjects)
+    random.shuffle(unassignedAgents)
+    remainingAssignments = zip(unassignedObjects, unassignedAgents)
+    for pair in remainingAssignments:
+        assignments.write('Resource ' + str(pair[0]) + ': ' + str(pair[1]) + '\n')
 
 
-resourceAllocation(10000, 100, 5, 678807)
+resourceAllocation(100, 140, 1, 678807)
 
 resourceChecker(100, 1)
+#will have to find where your document is saved on computer and use double backslashes
+RECA("C:\\Users\\Jake From State Farm\\Documents\\GitHub\\RICA-RECA\\new.txt")
